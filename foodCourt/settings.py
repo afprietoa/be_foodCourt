f148e3ec5c411c61595b6ca41cfa90e7b58a7a97
K-E-V-0 <kgonzalezg@unal.edu.co>
2023-10-17 19:38:26 -0500

    Create models for account users ** It is not migrated **

--- foodCourt/settings.py
+++ foodCourt/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -79,6 +80,10 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+
+# Configuración personalizada de autentificación
+AUTH_USER_MODEL = 'accounts.User'
 
 
 # Password validation
