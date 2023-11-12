583daf621766cec41123f5bc9d8c2408dead3cf6
K-E-V-0 <kgonzalezg@unal.edu.co>
2023-11-12 17:26:44 -0500

    accounts updated

--- accounts/models.py
+++ accounts/models.py
@@ -2,19 +2,20 @@ from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 from django.db.models.fields.related import ForeignKey, OneToOneField
 
+# Create your models here.
 class UserManager(BaseUserManager):
     def create_user(self, first_name, last_name, username, email, password=None):
         if not email:
-            raise ValueError('El usuario debe tener una correo electrónico.')
+            raise ValueError('User must have an email address')
 
         if not username:
-            raise ValueError('El usuario debe tener un Username.')
+            raise ValueError('User must have an username')
 
         user = self.model(
-            email=self.normalize_email(email),
-            username=username,
-            first_name=first_name,
-            last_name=last_name
+            email = self.normalize_email(email),
+            username = username,
+            first_name = first_name,
+            last_name = last_name,
         )
         user.set_password(password)
         user.save(using=self._db)
@@ -22,11 +23,11 @@ class UserManager(BaseUserManager):
 
     def create_superuser(self, first_name, last_name, username, email, password=None):
         user = self.create_user(
-            email=self.normalize_email(email),
-            username=username,
-            password=password,
-            first_name=first_name,
-            last_name=last_name
+            email = self.normalize_email(email),
+            username = username,
+            password = password,
+            first_name = first_name,
+            last_name = last_name,
         )
         user.is_admin = True
         user.is_active = True
@@ -42,17 +43,16 @@ class User(AbstractBaseUser):
 
     ROLE_CHOICE = (
         (VENDOR, 'Vendor'),
-        (CUSTOMER, 'Customer')
+        (CUSTOMER, 'Customer'),
     )
-
-    first_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
-    username = models.CharField(max_length=32, unique=True)
-    email = models.EmailField(max_length=64, unique=True)
-    phone_number = models.CharField(max_length=10, blank=True)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    username = models.CharField(max_length=50, unique=True)
+    email = models.EmailField(max_length=100, unique=True)
+    phone_number = models.CharField(max_length=12, blank=True)
     role = models.PositiveSmallIntegerField(choices=ROLE_CHOICE, blank=True, null=True)
 
-    # Datos requeridos
+    # required fields
     date_joined = models.DateTimeField(auto_now_add=True)
     last_login = models.DateTimeField(auto_now_add=True)
     created_date = models.DateTimeField(auto_now_add=True)
@@ -75,7 +75,7 @@ class User(AbstractBaseUser):
 
     def has_module_perms(self, app_label):
         return True
-    
+
     def get_role(self):
         if self.role == 1:
             user_role = 'Vendor'
@@ -83,12 +83,17 @@ class User(AbstractBaseUser):
             user_role = 'Customer'
         return user_role
 
+
 class UserProfile(models.Model):
     user = OneToOneField(User, on_delete=models.CASCADE, blank=True, null=True)
     profile_picture = models.ImageField(upload_to='users/profile_pictures', blank=True, null=True)
+    cover_photo = models.ImageField(upload_to='users/cover_photos', blank=True, null=True)
     address_line_1 = models.CharField(max_length=50, blank=True, null=True)
+    address_line_2 = models.CharField(max_length=50, blank=True, null=True)
     country = models.CharField(max_length=15, blank=True, null=True)
+    state = models.CharField(max_length=15, blank=True, null=True)
     city = models.CharField(max_length=15, blank=True, null=True)
+    pin_code = models.CharField(max_length=6, blank=True, null=True)
     latitude = models.CharField(max_length=20, blank=True, null=True)
     longitude = models.CharField(max_length=20, blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
